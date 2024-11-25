2b9766fc4c2ebc91d9623875c9b7fd5d68936bb7
oonisidesu <eeeelllleeee24@gmail.com>
2024-11-25 18:58:39 +0900

    norm対応

--- src/display/display_info.c
+++ src/display/display_info.c
@@ -6,13 +6,12 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 14:56:47 by ootsuboyosh       #+#    #+#             */
-/*   Updated: 2024/11/21 16:24:53 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/11/25 18:57:22 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "display.h"
 
-// テクスチャ情報を表示する関数
 void	display_textures(t_texture *textures)
 {
 	printf("North Texture: %p\n", (void *)textures->north.img);
@@ -23,7 +22,6 @@ void	display_textures(t_texture *textures)
 	printf("Ceiling Texture: %p\n", (void *)textures->ceiling.img);
 }
 
-// カラー情報を表示する関数
 void	display_colors(t_color *colors)
 {
 	printf("Floor Color: RGB(%d, %d, %d)\n", colors->floor[0], colors->floor[1],
@@ -32,17 +30,19 @@ void	display_colors(t_color *colors)
 		colors->ceiling[1], colors->ceiling[2]);
 }
 
-// マップデータを表示する関数
 void	display_map(t_map *map)
 {
+	int	row;
+
+	row = 0;
 	printf("Map Width: %d, Height: %d\n", map->width, map->height);
-	for (int i = 0; i < map->height; i++)
+	while (row < map->height)
 	{
-		printf("%s", map->data[i]);
+		printf("%s", map->data[row]);
+		row++;
 	}
 }
 
-// 全体のゲームデータを表示する関数
 void	display_cub3d(t_cub3d *cub3d)
 {
 	printf("=== Displaying Cub3D Data ===\n");
