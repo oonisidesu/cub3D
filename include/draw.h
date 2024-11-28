aa8d3519f228e6bf99a40ebb411810f6e1c35129
oonisidesu <eeeelllleeee24@gmail.com>
2024-11-28 16:33:03 +0900

    draw norm対応

--- include/draw.h
+++ include/draw.h
@@ -6,7 +6,7 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 16:12:04 by ootsuboyosh       #+#    #+#             */
-/*   Updated: 2024/11/28 16:18:27 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/11/28 16:27:47 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,21 @@
 
 typedef struct s_wall_params
 {
-	int	draw_start;
-	int	draw_end;
-	int	line_height;
-	int	x;
-}		t_wall_params;
+	int		draw_start;
+	int		draw_end;
+	int		line_height;
+	int		x;
+}			t_wall_params;
 
-void	draw_wall(t_game *game, t_ray *ray, t_wall_params *params);
-void	draw_floor_and_ceiling(t_game *game);
+typedef struct s_draw_params
+{
+	t_img	*texture;
+	int		tex_x;
+	double	step;
+	double	tex_pos;
+}			t_draw_params;
+
+void		draw_wall(t_game *game, t_ray *ray, t_wall_params *params);
+void		draw_floor_and_ceiling(t_game *game);
 
 #endif
--- src/draw/draw_wall.c
+++ src/draw/draw_wall.c
@@ -6,47 +6,78 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:14:17 by ootsuboyosh       #+#    #+#             */
-/*   Updated: 2024/11/28 16:19:08 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/11/28 16:32:33 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "game.h"
 #include "draw.h"
+#include "game.h"
 #include "util.h"
 #include <math.h>
 
-void	draw_wall(t_game *game, t_ray *ray, t_wall_params *params)
+t_img	*select_texture(t_game *game, t_ray *ray)
 {
-	t_img	*texture;
-	int		color;
-	double	wall_x;
-
-	int tex_x, tex_y;
-	double step, tex_pos;
-	// 壁に応じたテクスチャを選択
 	if (ray->side == 0)
-		texture = (ray->step_x < 0) ? &game->game_data.textures.west : &game->game_data.textures.east;
+	{
+		if (ray->step_x < 0)
+			return (&game->game_data.textures.west);
+		else
+			return (&game->game_data.textures.east);
+	}
 	else
-		texture = (ray->step_y < 0) ? &game->game_data.textures.north : &game->game_data.textures.south;
-	// 壁のX座標を計算（テクスチャ内）
-	wall_x = (ray->side == 0) ? ray->map_y + ray->perp_wall_dist
-		* ray->ray_dir_y : ray->map_x + ray->perp_wall_dist * ray->ray_dir_x;
+	{
+		if (ray->step_y < 0)
+			return (&game->game_data.textures.north);
+		else
+			return (&game->game_data.textures.south);
+	}
+}
+
+int	cal_tex_x(t_ray *ray, t_img *texture)
+{
+	double	wall_x;
+	int		tex_x;
+
+	if (ray->side == 0)
+		wall_x = ray->map_y + ray->perp_wall_dist * ray->ray_dir_y;
+	else
+		wall_x = ray->map_x + ray->perp_wall_dist * ray->ray_dir_x;
 	wall_x -= floor(wall_x);
 	tex_x = (int)(wall_x * texture->width);
 	if ((ray->side == 0 && ray->ray_dir_x > 0) || (ray->side == 1
 			&& ray->ray_dir_y < 0))
 		tex_x = texture->width - tex_x - 1;
-	// テクスチャの縦方向ステップ
-	step = 1.0 * texture->height / params->line_height;
-	tex_pos = (params->draw_start - WINDOW_HEIGHT / 2 + params->line_height / 2)
-		* step;
-	// 壁を描画
-	for (int y = params->draw_start; y < params->draw_end; y++)
+	return (tex_x);
+}
+
+void	draw_wall_texture(t_game *game, t_wall_params *params,
+		t_draw_params *draw_params)
+{
+	int	y;
+	int	tex_y;
+	int	color;
+
+	y = params->draw_start;
+	while (y < params->draw_end)
 	{
-		tex_y = (int)tex_pos & (texture->height - 1);
-		tex_pos += step;
-		color = *(int *)(texture->addr + (tex_y * texture->line_length + tex_x
-					* (texture->bits_per_pixel / 8)));
+		tex_y = (int)draw_params->tex_pos & (draw_params->texture->height - 1);
+		draw_params->tex_pos += draw_params->step;
+		color = *(int *)(draw_params->texture->addr + (tex_y
+					* draw_params->texture->line_length + draw_params->tex_x
+					* (draw_params->texture->bits_per_pixel / 8)));
 		put_pixel_to_image(game, params->x, y, color);
+		y++;
 	}
+}
+
+void	draw_wall(t_game *game, t_ray *ray, t_wall_params *params)
+{
+	t_draw_params	draw_params;
+
+	draw_params.texture = select_texture(game, ray);
+	draw_params.tex_x = cal_tex_x(ray, draw_params.texture);
+	draw_params.step = 1.0 * draw_params.texture->height / params->line_height;
+	draw_params.tex_pos = (params->draw_start - WINDOW_HEIGHT / 2
+			+ params->line_height / 2) * draw_params.step;
+	draw_wall_texture(game, params, &draw_params);
 }
