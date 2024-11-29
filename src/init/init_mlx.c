a5b1c558614b7752b19d006e5620f1b479e1db3b
oonisidesu <eeeelllleeee24@gmail.com>
2024-11-29 16:43:36 +0900

    ちょっとスッキリ整形

--- src/init/init_mlx.c
+++ src/init/init_mlx.c
@@ -6,47 +6,26 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:47:36 by ootsuboyosh       #+#    #+#             */
-/*   Updated: 2024/11/28 14:14:14 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/11/29 16:42:31 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "init.h"
-
-void	init_graphic(t_game *game)
-{
-	game->mlx = mlx_init();
-	if (!game->mlx)
-	{
-		perror("Error initializing MinilibX");
-		exit(EXIT_FAILURE);
-	}
-	game->win = mlx_new_window(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "cub3D");
-	if (!game->win)
-	{
-		perror("Error creating window");
-		exit(EXIT_FAILURE);
-	}
-}
-
-void	init_image(t_game *game)
-{
-	game->img.img = mlx_new_image(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	if (!game->img.img)
-	{
-		perror("Error creating image");
-		exit(EXIT_FAILURE);
-	}
-	game->img.addr = mlx_get_data_addr(game->img.img, &game->img.bits_per_pixel,
-			&game->img.line_length, &game->img.endian);
-	if (!game->img.addr)
-	{
-		perror("Error getting image address");
-		exit(EXIT_FAILURE);
-	}
-}
+#include "util.h"
 
 void	init_mlx(t_game *game)
 {
-	init_graphic(game);
-	init_image(game);
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		print_error_exit("initializing MinilibX\n");
+	game->win = mlx_new_window(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "cub3D");
+	if (!game->win)
+		print_error_exit("creating window\n");
+	game->img.img = mlx_new_image(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	if (!game->img.img)
+		print_error_exit("creating image\n");
+	game->img.addr = mlx_get_data_addr(game->img.img, &game->img.bits_per_pixel,
+			&game->img.line_length, &game->img.endian);
+	if (!game->img.addr)
+		print_error_exit("getting image address\n");
 }
--- src/util/util_error.c
+++ src/util/util_error.c
@@ -6,7 +6,7 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:55:25 by ootsuboyosh       #+#    #+#             */
-/*   Updated: 2024/11/28 15:56:16 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/11/29 16:42:53 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,17 @@
 void	display_usage_and_exit(const char *program_name)
 {
 	fprintf(stderr, "Usage: %s <map.cub>\n", program_name);
+	exit(EXIT_FAILURE);
+}
+
+void	print_error(const char *message)
+{
+	printf("Error\n");
+	perror(message);
+}
+
+void	print_error_exit(const char *message)
+{
+	print_error(message);
 	exit(EXIT_FAILURE);
 }
