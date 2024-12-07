79eebd02b5f4ca0f193d5b269b93982b8d5864db
oonisidesu <eeeelllleeee24@gmail.com>
2024-12-07 15:47:16 +0900

    errorcaseはエラーが出るようにテスト作成

--- include/util.h
+++ include/util.h
@@ -6,7 +6,7 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 16:50:31 by ootsuboyosh       #+#    #+#             */
-/*   Updated: 2024/12/06 17:57:13 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/12/07 15:44:55 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 void		put_pixel_to_image(t_game *game, int x, int y, int color);
 void		load_texture(t_game *game, t_img *texture, char *path);
 char		**resize_array(char **old_array, int old_size, int new_size);
-void		display_usage_and_exit(const char *program_name);
 void		print_error(const char *message);
 void		print_error_exit(const char *message);
 void		print_error_free(const char *message, t_game *game);
--- src/util/util_error.c
+++ src/util/util_error.c
@@ -6,22 +6,21 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:55:25 by ootsuboyosh       #+#    #+#             */
-/*   Updated: 2024/11/30 16:21:44 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/12/07 15:44:59 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/util.h"
-
-void	display_usage_and_exit(const char *program_name)
-{
-	fprintf(stderr, "Usage: %s <map.cub>\n", program_name);
-	exit(EXIT_FAILURE);
-}
+#include "libft.h"
+#include <unistd.h>
 
 void	print_error(const char *message)
 {
-	printf("Error\n");
-	perror(message);
+	char	*error_prefix;
+
+	error_prefix = "Error\n";
+	write(STDERR_FILENO, error_prefix, ft_strlen(error_prefix));
+	write(STDERR_FILENO, message, ft_strlen(message));
 }
 
 void	print_error_exit(const char *message)
@@ -32,8 +31,7 @@ void	print_error_exit(const char *message)
 
 void	print_error_free(const char *message, t_game *game)
 {
-	printf("Error\n");
-	perror(message);
+	print_error(message);
 	free_game(game);
 }
 
