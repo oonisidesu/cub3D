50d2fbadd3d855ac82c783c6f0785e1e2c6c2cf2
oonisidesu <eeeelllleeee24@gmail.com>
2024-12-12 17:41:30 +0900

    ビルドできなかったので対応

--- include/validate.h
+++ include/validate.h
@@ -6,7 +6,7 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 18:13:53 by ootsuboyosh       #+#    #+#             */
-/*   Updated: 2024/12/12 17:33:35 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/12/12 17:40:39 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ bool	validate_line(const char *line, t_game *game);
 bool	validate_map(t_map *map);
 void	validate_cub_file(t_game *game, t_cub_el *cub_el_flag);
 bool	check_map_characters(t_map *map);
+bool	check_player_position(t_map *map);
 bool	check_map_closed(t_map *map);
 bool	has_two_or_more_walls_in_row(char *row);
 bool	has_two_or_more_walls_in_column(char **map, size_t column_index,
--- src/validate/validate_map_wall.c
+++ src/validate/validate_map_wall.c
@@ -6,7 +6,7 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 18:27:28 by ootsuboyosh       #+#    #+#             */
-/*   Updated: 2024/12/12 17:37:24 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/12/12 17:40:58 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ static bool	is_valid_player_char(t_map *map, size_t i, size_t j)
 	return (ft_strchr(PLAYER_CHARS, map->data[i][j]) != NULL);
 }
 
-static bool	check_player_position(t_map *map, char **visited)
+static bool	analyze_player_position(t_map *map, char **visited)
 {
 	size_t	i;
 	size_t	j;
@@ -87,7 +87,7 @@ bool	check_map_closed(t_map *map)
 	visited = allocate_visited_map(map);
 	if (!visited)
 		return (false);
-	if (!check_player_position(map, visited))
+	if (!analyze_player_position(map, visited))
 	{
 		free_visited_map(visited, map->height);
 		return (false);
