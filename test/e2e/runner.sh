419fe85109520271fcb8b716035fe4cc6b3faa3e
yu-432 <o.shimayu891@gmail.com>
2024-12-21 13:29:00 +0900

    :sparkles: 存在しないファイルをテストケースに追加

--- test/e2e/runner.sh
+++ test/e2e/runner.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-EXECUTABLE="./cub3D"
+EXECUTABLE="../.././cub3D"
 
-MAP_DIR="map/error"
+MAP_DIR="../../map/error"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
+TEMP_FILE=$(mktemp)
+trap 'rm -f $TEMP_FILE' EXIT
+
 echo -e "${YELLOW}Starting e2e ErrorCaseTest ...${NC}"
 echo "Executable: $EXECUTABLE"
 echo "Testing directory: $MAP_DIR"
 echo "--------------------------------------"
 
-for file in "$MAP_DIR"/*.cub; do
-    if [ -f "$file" ]; then
-        echo -e "Running: ${GREEN}$EXECUTABLE $file${NC}"
-        
+find "$MAP_DIR" -name "*.cub" > $TEMP_FILE
+echo "$MAP_DIR/DOES_NOT_EXIST.cub" >> $TEMP_FILE
+
+while read file; do
+    echo -e "Running: ${GREEN}$EXECUTABLE $file${NC}"
         OUTPUT=$($EXECUTABLE "$file" 2>&1)
         RETURN_CODE=$?
-
+        
         if [ $RETURN_CODE -eq 0 ]; then
             echo -e "${GREEN}Success: ${NC}$file passed the test."
         else
@@ -28,11 +32,7 @@ for file in "$MAP_DIR"/*.cub; do
             echo -e "${RED}Output: ${NC}"
             echo "$OUTPUT" | sed "s/^/    /"
         fi
-        echo "--------------------------------------"
-    else
-        echo -e "${RED}No .cub files found in $MAP_DIR${NC}"
-        exit 1
-    fi
-done
+    echo "--------------------------------------"
+done < "$TEMP_FILE"
 
 echo -e "${YELLOW}All tests completed.${NC}"
